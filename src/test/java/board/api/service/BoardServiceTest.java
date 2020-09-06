3823cad9b9d272643af0a87a89142beeb85e215e
siroshtan <siroshtan.home@gmail.com>
2020-09-06 18:56:15 +0300

    add

---
+++ src/test/java/board/api/service/BoardServiceTest.java
@@ -0,0 +1,28 @@
+package board.api.service;
+
+import junit.framework.TestCase;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class BoardServiceTest extends TestCase {
+
+
+    @BeforeClass
+    public static void beforeClass() throws UnknownHostException {
+    }
+
+    @AfterClass
+    public static void afterClass() throws IOException {
+    }
+
+    @Test
+    public void testAddBoard() {
+
+    }
+}
